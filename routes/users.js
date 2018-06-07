b7248a899aa1f87d89ec25ee9c8afa0a7f4ac968
Long Nguyen The Phung <thelonglqd@gmail.com>
2018-06-07 17:19:46 +0700

    implement put method for orders

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,35 @@ router.get('/:id/orders', function(req, res, next) {
     } else {
       data = JSON.parse(data.toString());
       var orders = data.filter(user => user.id == req.params.id);
+      res.status(200);
       return res.send(data.filter(user => user.id == req.params.id)[0].orders)
+    }
+  })
+});
+
+router.put('/:id/orders', function (req, res, next) {
+  console.log('req.body: ', req.body);
+  console.log('req.params: ', req.params);
+  fs.readFile('db.json', (err, data) => {
+    if (err) {
+      res.status(500);
+      return res.send(err);
+    } else {
+      data = JSON.parse(data.toString());
+      data.forEach(user => {
+        if (user.id == req.params.id) {
+          user.orders.push({size: parseFloat(req.body.size), bid: parseFloat(req.body.bid) });
+        }
+      });
+      fs.writeFile('db.json', JSON.stringify(data), (err) => {
+        if (err) {
+          res.status(500);
+          return res.send({ errorMessage: 'Internal server error' })
+        } else {
+          res.status(201);
+          return res.send({ message: 'ok' });
+        }
+      })
     }
   })
 });
