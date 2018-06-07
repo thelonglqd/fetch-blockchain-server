2b0e710bbff426f798bfc535b0993e7ff21eb098
Long Nguyen The Phung <thelonglqd@gmail.com>
2018-06-07 16:11:59 +0700

    add json file

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var _ = require('lodash');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  console.log('req.body: ', req.body);
-  res.send({ test: 'LOGIN API '});
+  const { username, password } = req.body;
+  fs.readFile('db.json', (err, data) => {
+    if (err) {
+      res.status(500);
+      return res.send(err);
+    } else {
+      data = JSON.parse(data.toString());
+      console.log(data.filter(user => user.username === username && user.password === password))
+      if (data.filter(user => user.username === username && user.password === password).length === 1) {
+        res.status(200);
+        res.send(data.filter(user => user.username === username && user.password === password));
+      }
+    }
+    
+  });
 });
 
 module.exports = router;
