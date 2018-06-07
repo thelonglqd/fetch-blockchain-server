422a598fd291645eb1628f4d071068b5aace3d87
Long Nguyen The Phung <thelonglqd@gmail.com>
2018-06-07 16:42:28 +0700

    implement get orders of user api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id/orders', function(req, res, next) {
+  fs.readFile('db.json', (err, data) => {
+    if (err) {
+      res.status(500);
+      return res.send(err);
+    } else {
+      data = JSON.parse(data.toString());
+      var orders = data.filter(user => user.id == req.params.id);
+      return res.send(data.filter(user => user.id == req.params.id)[0].orders)
+    }
+  })
 });
 
 module.exports = router;
